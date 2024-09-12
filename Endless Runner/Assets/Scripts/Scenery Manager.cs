294aff54a863cd7f0d8a19563a1fb28e6d5be1c6
tallestcat001 <shdmlgus123_@naver.com>
2024-09-12 19:43:45 +0900

    (2024.9.12) Runner Game 3

--- Endless Runner/Assets/Scripts/Scenery Manager.cs
+++ Endless Runner/Assets/Scripts/Scenery Manager.cs
@@ -2,21 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image screenInage;
+
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(gameObject);  
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;    
+    }
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(1);
+            StartCoroutine(AsyncLoad(1));
         }
+    }
+
+    public IEnumerator FadeIn()
+    {
+        screenInage.gameObject.SetActive(true);
+
+        Color color = screenInage.color;
+
+        color.a = 1f;
+
+        while(color.a >= 0.0f)
+        {
+            color.a -= Time.deltaTime;
+
+            screenInage.color = color;
+
+            yield return null;
+        }
+
+        screenInage.gameObject.SetActive(false);
+    }
+
+    public IEnumerator AsyncLoad(int index)
+    {
+        screenInage.gameObject.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+
+        asyncOperation.allowSceneActivation = false;
+        // allowSceneActivation : 장면이 준비된 즉시 장면이 활성화되는 것을 허용하는 변수.
+
+        Color color = screenInage.color;
+
+        color.a = 0;
+
+        while(asyncOperation.isDone == false)
+        // isDone : 해당 동적이 완료되었는지 나타내는 변수. (읽기 전용)
+        {
+            color.a += Time.deltaTime;
+
+            screenInage.color = color;
+
+            if(asyncOperation.progress >= 0.9f)
+            // progress : 작업의 진행 상태를 나타내는 변수. (읽기 전용)
+            {
+                color.a = Mathf.Lerp(color.a,1f,Time.deltaTime);
+
+                screenInage.color = color;
+
+                if(color.a >= 1.0f)
+                {
+                    asyncOperation.allowSceneActivation = true;
+
+                    yield break;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    void OnSceneLoaded(Scene scene,LoadSceneMode loadSceneMode)
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
